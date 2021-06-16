4de8841c5db134b3a2d5e54d5428556031c3983d
Andy Wei Kit Liu <andyweikitliu@andys-air.attlocal.net>
2021-06-16 12:55:01 -0700

    lab 3- step x

--- fundamentals_practice.py
+++ fundamentals_practice.py
@@ -2,15 +2,15 @@ print("Hello World!")
 # the terminal will return Hello World!
 
 #Step 1 
-# the variable's datatype on line 4 is a String(Word)
+# the variable's datatype on line 4 is a String(Text Sequence)
 name = "LeBron"
-# the variable's datatype on line 6 is a integer
+# the variable's datatype on line 6 is a integer(Numeric)
 age = 35
-# the variable's datatype on line 8 is a decimal(double)
+# the variable's datatype on line 8 is a decimal(Numeric)
 salary = 37.44
 # the variable's datatype on line 10 is a boolean(true or false)
 plays_basketball = True
-# the variable's datatype on line 12 is a String
+# the variable's datatype on line 12 is a String(Text Sequence)
 jersey_number = "23"
 
 #Step 2 
