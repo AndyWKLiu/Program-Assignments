fa0b2a7a6ab76fc719baa7e9259ca4b913299fc9
AndyWKLiu <liuandy606@gmail.com>
2021-06-28 12:06:01 -0700

    Python Worksheet

---
+++ loopcounter.py
@@ -0,0 +1,6 @@
+#Problem 1
+for x in range(0, 20):
+    value = input("Enter a number between 1 and 10 inclusive")
+    if(value > 0 and value < 11):
+        Calculate = value * 3
+        print(Calculate)
